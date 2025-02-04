3243d741412503328bedbfa2a42cb70998d3da3f
mfcastilho <fredericocastilho@hotmail.com>
2025-02-04 16:28:05 -0300

    feat: Card component finalized

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import './Card.scss';
+
+type Props = {
+  title: string;
+  description: string;
+  photoSide?: 'LEFT' | 'RIGHT';
+  cardImage: string;
+}
+
+export const Card: React.FC<Props> = ({ cardImage, description, photoSide, title }) => {
+  return (
+    <div className="Card">
+      <a
+        href="https://www.youtube.com/watch?v=S2V36swLbnw"
+        target="_blank"
+        className={`Card__content  Section__content ${photoSide === 'LEFT' ? 'Card__content--left' : ''}`}
+      >
+
+        {photoSide === 'LEFT' ? (
+          <>
+            <div className="Card__image-box">
+              <img
+                src={cardImage}
+                alt={title}
+                className="Card__image"
+              />
+            </div>
+
+            <div className="Card__infos-wrapper">
+              <h3 className="Card__title">{title}</h3>
+              <p className="Card__description">{description}</p>
+            </div>
+
+          </>
+        ) : (
+          <>
+            <div className={`Card__infos-wrapper ${photoSide === 'RIGHT' ? 'Card__infos-wrapper--right' : ''}`}>
+              <h3 className="Card__title">{title}</h3>
+              <p className="Card__description">{description}</p>
+            </div>
+
+            <div className="Card__image-box">
+              <img
+                src={cardImage}
+                alt={title}
+                className="Card__image"
+              />
+            </div>
+          </>
+        )}
+      </a>
+    </div>
+  );
+}
+
