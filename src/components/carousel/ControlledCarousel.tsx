5bea28e1688d3068d3a8e1beb30ee9e33ed95301
mfcastilho <fredericocastilho@hotmail.com>
2025-02-04 16:22:52 -0300

    feat: ControlledCarousel component finalized

--- src/components/carousel/ControlledCarousel.tsx
+++ src/components/carousel/ControlledCarousel.tsx
@@ -5,23 +5,23 @@ import './ControlledCarousel.scss';
 const bannerImages = [
   {
     id: 1,
-    title: 'First slide label',
-    description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-    url: 'https://coletivocultural.redelivre.org.br/files/2019/01/photo_2018-06-24_13-32-44.jpg',
+    title: 'Coletivo Cafeína',
+    description: '',
+    url: '../../../public/img/banner-1-site-coletivo.png',
     alt_description: 'First slide image',
   },
   {
     id: 2,
-    title: 'Second slide label',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    url: 'https://coletivocultural.redelivre.org.br/files/2019/02/41693997834_4affab3184_k.jpg',
+    title: 'Documentários',
+    description: '',
+    url: '../../../public/img/doc-caninha-verde.png',
     alt_description: 'Second slide image',
   },
   {
     id: 3,
-    title: 'Third slide label',
-    description: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-    url: 'https://coletivocultural.redelivre.org.br/files/2019/02/31548908837_3c49740f4c_b.jpg',
+    title: 'Cultura',
+    description: '',
+    url: 'https://coletivocultural.redelivre.org.br/files/2019/02/41693997834_4affab3184_k.jpg',
     alt_description: 'Third slide image',
   },
 ];
@@ -40,7 +40,6 @@ export function ControlledCarousel() {
           <img className="d-block w-100" src={image.url} alt={image.alt_description} />
           <Carousel.Caption>
             <h3>{image.title}</h3>
-            <p>{image.description}</p>
           </Carousel.Caption>
         </Carousel.Item>
       ))}
