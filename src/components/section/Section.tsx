d75ff19b2df954357c4209be42ef73d1ffbe3e18
mfcastilho <fredericocastilho@hotmail.com>
2025-02-04 16:25:50 -0300

    feat: Section component finalized

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -4,15 +4,44 @@ import './Section.scss';
 type Props = {
   title: string;
   text?: string;
+  side?: 'LEFT' | 'RIGHT';
+  imageUrl?: string;
 }
 
-export const Section: React.FC<Props> = ({ title, text }) => {
+export const Section: React.FC<Props> = ({ title, text, side, imageUrl }) => {
   return (
-    <div className="Section">
-      <section className="Home__section Section__content">
-        <h2 className="Section__title">{title}</h2>
-        <p className="Section_text">{text}</p>
-      </section>
-    </div>
+    <>
+      {title !== 'Projetos' ? (
+        <div className="Section">
+          <section className={`Home__section Section__content ${side === 'LEFT' ? 'Section__content--left' : ''}`}>
+            {side === 'LEFT' ? (
+              <>
+                <img src={imageUrl} alt="" className="Section__img" />
+
+                <div className="Section__infos-box">
+                  <h2 className="Section__title">{title}</h2>
+                  <p className="Section_text">{text}</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="Section__infos-box">
+                  <h2 className="Section__title">{title}</h2>
+                  <p className="Section_text">{text}</p>
+                </div>
+
+                <img src={imageUrl} alt="" className="Section__img" />
+              </>
+            )}
+          </section>
+        </div>
+      ) : (
+        <div className="Section">
+          <section className={`Card__section`}>
+            <h2 className="Section__title">{title}</h2>
+          </section>
+        </div>
+      )}
+    </>
   );
-}
+};
